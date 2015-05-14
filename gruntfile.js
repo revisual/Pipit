b32571bb3322d8f449bd9c3c3b43cfc5fe49d20c
neilmanuell <neil.revisual@gmail.com>
2015-05-14 17:12:34 +0100

    adds grunt-bump task

--- gruntfile.js
+++ gruntfile.js
@@ -72,22 +72,31 @@ module.exports = function ( grunt ) {
                   /*captureFile: 'results.txt', // Optionally capture the reporter output to a file*/
                   quiet: false, // Optionally suppress output to standard out (defaults to false)
                   clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false)
-                  ui:'bdd'
+                  ui: 'bdd'
                },
                src: [
                   'test/server/*.js'
                ]
             }
-         } ,
+         },
          karma: {
-           /* unit: {
-               configFile: 'karma.conf.js'
-            }   ,*/
+            /* unit: {
+             configFile: 'karma.conf.js'
+             }   ,*/
             continuous: {
                configFile: 'karma.conf.js',
                singleRun: true,
                autoWatch: false
             }
+         },
+         bump: {
+
+               options: {
+                  files: ['package.json'],
+                  push: true,
+                  pushTo: 'origin'
+               }
+
          }
 
       } );
@@ -97,11 +106,13 @@ module.exports = function ( grunt ) {
    grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
    grunt.loadNpmTasks( 'grunt-contrib-uglify' );
    grunt.loadNpmTasks( 'grunt-htmlclean' );
-   grunt.loadNpmTasks('grunt-mocha-test');
-   grunt.loadNpmTasks('grunt-karma');
+   grunt.loadNpmTasks( 'grunt-mocha-test' );
+   grunt.loadNpmTasks( 'grunt-karma' );
+   grunt.loadNpmTasks( 'grunt-bump' );
 
 
    grunt.registerTask( 'default', ['clean:all', 'copy', 'htmlclean:deploy', 'cssmin', 'uglify'] );
-   grunt.registerTask( 'build', ['mochaTest', 'karma','clean:all', 'copy', 'htmlclean:deploy', 'cssmin', 'uglify'] );
-   grunt.registerTask('tests', ['mochaTest','karma']);
+   grunt.registerTask( 'build', ['mochaTest', 'karma', 'clean:all', 'copy', 'htmlclean:deploy', 'cssmin', 'uglify'] );
+   grunt.registerTask( 'tests', ['mochaTest', 'karma'] );
+   grunt.registerTask( 'pushDev', ['bump'] );
 };
